76b68fada2e802cdd0609b89a6f15f3f39a27e0b
Nishanth D <70720880+Nishanth21D@users.noreply.github.com>
2024-03-15 13:39:11 +0000

    Update custom_snapchat_1.py
    Adding of extra filter and options of (Prev & Next)

--- Py-Scripts/Custom Snapchat Filter/custom_snapchat_1.py
+++ Py-Scripts/Custom Snapchat Filter/custom_snapchat_1.py
@@ -37,6 +37,10 @@ filter_config = {
     'anonymous':
         [{'path': "filters/GrassVillage_Anonymous_Halloween.png",
           'anno_path': "filters/GrassVillage_Anonymous_Halloween_Annotation.csv",
+          'morph': True}],
+    'joker':
+        [{'path': "filters/joker.png",
+          'anno_path': "filters/joker_annotation.csv",
           'morph': True}]
 }
 # os.listdir("Github Repos/Create-AR-filters-using-Mediapipe/filters/anonymous_annotations.csv")
@@ -101,6 +105,34 @@ def find_convex_hull(points):
     return hull, hull_indx
 
 
+def get_landmarks(frame):
+    results = face_mesh.process(frame)  # Applying Facemesh
+    if not results.multi_face_landmarks:
+        print("Face Not Detected")
+        return 0
+    if results.multi_face_landmarks:
+        for lmark in results.multi_face_landmarks:
+            xlist, ylist = [], []                       # For BBOX
+            face_keypoints = []                         # Contains all face points
+            for idx, l_mark in enumerate(lmark.landmark):
+                h, w = frame.shape[:2]
+                px, py = int(l_mark.x * w), int(l_mark.y * h)
+                xlist.append(px)
+                ylist.append(py)
+                face_keypoints.append((px,py))
+                if idx in selected_keypoints:
+                    cv2.circle(frame,(px,py),1,(0,0,255),-1)    # Landmark points
+                # cv2.putText(frame,str(idx),(cx,cy),1,0.5,(255,0,0),1)        # Landmark labels
+                # if idx == 151:
+                #     cv2.circle(frame, (px, py), 4, (255, 255, 0), -1)
+
+            relevant_keypoints = []
+            for i in selected_keypoints:
+                relevant_keypoints.append(face_keypoints[i])  # Extract the coordinates of selected keypoints
+            return relevant_keypoints, xlist, ylist
+    return 0
+
+
 def load_filter(filter_name='anonymous'):
     filters = filter_config[filter_name]
     filter_runtime = []
@@ -141,14 +173,17 @@ frst_frame = True
 flt_indx = False
 sigma = 50
 
-filter_keys = iter(filter_config.keys())
-# filters, filter_runtime = load_filter(next(filter_keys))
-filters, multi_filter_runtime = load_filter('anonymous')
+filter_keys = [key for key in filter_config.keys()]
+key_length = len(filter_keys)
+index = 0
 
+filters, multi_filter_runtime = load_filter(filter_keys[index])
+# filters, multi_filter_runtime = load_filter('anonymous')
 cap = cv2.VideoCapture(0)
 
 # Main Loop
 while cv2.waitKey(1) != 27:
+    # print(indx)
     has_frame, frame = cap.read()
     if not has_frame:
         break
@@ -162,124 +197,112 @@ while cv2.waitKey(1) != 27:
     # To improve performance, making the image as not writable
     frame.flags.writeable = False
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    results = face_mesh.process(frame)  # Applying Facemesh
+
 
     # Making the image as writable now
     frame.flags.writeable = True
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-    if not results.multi_face_landmarks:
-        print("Face Not Detected")
-    if results.multi_face_landmarks:
-        for lmark in results.multi_face_landmarks:
-            xlist, ylist = [], []                       # For BBOX
-            face_keypoints = []                         # Contains all face points
-            for idx, l_mark in enumerate(lmark.landmark):
-                h, w = frame.shape[:2]
-                px, py = int(l_mark.x * w), int(l_mark.y * h)
-                xlist.append(px)
-                ylist.append(py)
-                face_keypoints.append((px,py))
-                if idx in selected_keypoints:
-                    cv2.circle(frame,(px,py),1,(0,0,255),-1)    # Landmark points
-                # cv2.putText(frame,str(idx),(cx,cy),1,0.5,(255,0,0),1)        # Landmark labels
-                # if idx == 151:
-                #     cv2.circle(frame, (px, py), 4, (255, 255, 0), -1)
+    # Get Facial Landmarks from Mediapipe Facemesh
+    relevant_keypoints, xlist, ylist = get_landmarks(frame)
 
-            relevant_keypoints = []
-            for i in selected_keypoints:
-                relevant_keypoints.append(face_keypoints[i])            # Extract the coordinates of selected keypoints
+    if not relevant_keypoints or (len(relevant_keypoints) < (len(relevant_keypoints)-30)):
+        continue
+    ###### ----- Optical flow & Stabilization ------- #######
+    frame_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    if frst_frame:
+        prev_points = np.array(relevant_keypoints, np.float32)
+        # print(prev_points)
+        frame_gray_prev = np.copy(frame_gray)
+        frst_frame = False
 
-            if not relevant_keypoints or (len(relevant_keypoints) < (len(relevant_keypoints)-30)):
-                continue
+    # Calculating Optical Flow
+    next_points, err, sts = cv2.calcOpticalFlowPyrLK(frame_gray_prev,frame_gray, prev_points,
+                                                     np.array(relevant_keypoints, np.float32), **lk_params)
+    for k in range(0, len(relevant_keypoints)):
+        d = cv2.norm(np.array(relevant_keypoints[k]- next_points[k]))
+        alpha = math.exp(-d * d / sigma)
+        relevant_keypoints[k] = (1 - alpha) * np.array(relevant_keypoints[k]) + alpha * next_points[k]
+        relevant_keypoints[k] = constrainpoint(relevant_keypoints[k], frame.shape[1], frame.shape[0])
+        relevant_keypoints[k] = (int(relevant_keypoints[k][0]), int(relevant_keypoints[k][1]))
 
-            ###### ----- Optical flow & Stabilization ------- #######
-            frame_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            if frst_frame:
-                prev_points = np.array(relevant_keypoints, np.float32)
-                # print(prev_points)
-                frame_gray_prev = np.copy(frame_gray)
-                frst_frame = False
+    # update variable for next pass
+    prev_points = np.array(relevant_keypoints, np.float32)
+    frame_gray_prev = frame_gray
+    ###### ----- End of Optical flow & Stabilization ------- #######
 
-            # Calculating Optical Flow
-            next_points, err, sts = cv2.calcOpticalFlowPyrLK(frame_gray_prev,frame_gray, prev_points,
-                                                             np.array(relevant_keypoints, np.float32), **lk_params)
-            for k in range(0, len(relevant_keypoints)):
-                d = cv2.norm(np.array(relevant_keypoints[k]- next_points[k]))
-                alpha = math.exp(-d * d / sigma)
-                relevant_keypoints[k] = (1 - alpha) * np.array(relevant_keypoints[k]) + alpha * next_points[k]
-                relevant_keypoints[k] = constrainpoint(relevant_keypoints[k], frame.shape[1], frame.shape[0])
-                relevant_keypoints[k] = (int(relevant_keypoints[k][0]), int(relevant_keypoints[k][1]))
+    if flt_indx:
+        # print("Inside Filter Loop")
+        for indx, filt in enumerate(filters):
+            filter_runtime = multi_filter_runtime[indx]
+            img = filter_runtime['img']
+            img_alpha = filter_runtime['img_alpha']
+            points = filter_runtime['points']
 
-            # update variable for next pass
-            prev_points = np.array(relevant_keypoints, np.float32)
-            frame_gray_prev = frame_gray
-            ###### ----- End of Optical flow & Stabilization ------- #######
+            if filt['morph']:
+                # print("Inside Morph Loop")
+                hull1 = filter_runtime['hull']
+                hull_indx = filter_runtime['hull_indx']
+                dt = filter_runtime['dt']
 
-            if flt_indx:
-                print("Inside Filter Loop")
-                for indx, filt in enumerate(filters):
-                    filter_runtime = multi_filter_runtime[indx]
-                    img = filter_runtime['img']
-                    img_alpha = filter_runtime['img_alpha']
-                    points = filter_runtime['points']
+                warped_img = np.copy(frame)
+                hull2 = []
+                for i in range(0, len(hull_indx)):
+                    hull2.append(relevant_keypoints[hull_indx[i][0]])
+                mask1 = np.zeros((warped_img.shape[0], warped_img.shape[1]), dtype=np.float32)
+                mask1 = cv2.merge((mask1,mask1,mask1))
+                img_alpha_mask = cv2.merge((img_alpha,img_alpha,img_alpha))
 
-                    if filt['morph']:
-                        print("Inside Morph Loop")
-                        hull1 = filter_runtime['hull']
-                        hull_indx = filter_runtime['hull_indx']
-                        dt = filter_runtime['dt']
+                # Warp the Triangle
+                for i in range(0, len(dt)):
+                    t1, t2 = [], []
+                    for j in range(0,3):
+                        t1.append(hull1[dt[i][j]])
+                        t2.append(hull2[dt[i][j]])
+                    fbc.warpTriangle(img, warped_img, t1, t2)
+                    fbc.warpTriangle(img_alpha_mask, mask1, t1, t2)
 
-                        warped_img = np.copy(frame)
-                        hull2 = []
-                        for i in range(0, len(hull_indx)):
-                            hull2.append(relevant_keypoints[hull_indx[i][0]])
-                        mask1 = np.zeros((warped_img.shape[0], warped_img.shape[1]), dtype=np.float32)
-                        mask1 = cv2.merge((mask1,mask1,mask1))
-                        img_alpha_mask = cv2.merge((img_alpha,img_alpha,img_alpha))
+                # Blur the mask before blending
+                mask1 = cv2.GaussianBlur(mask1,(3,3), 10)
+                mask2 = (255.0, 255.0, 255.0) - mask1
 
-                        # Warp the Triangle
-                        for i in range(0, len(dt)):
-                            t1, t2 = [], []
-                            for j in range(0,3):
-                                t1.append(hull1[dt[i][j]])
-                                t2.append(hull2[dt[i][j]])
-                            fbc.warpTriangle(img, warped_img, t1, t2)
-                            fbc.warpTriangle(img_alpha_mask, mask1, t1, t2)
+                # Perform alpha blending of the two images
+                temp1 = np.multiply(warped_img, (mask1 * (1.0/255)))
+                temp2 = np.multiply(frame, (mask2 * (1.0 / 255)))
+                output = temp1 + temp2
+                cv2.imshow('output', temp1)
+                frame = output = np.uint8(output)
+            else:
+                pass
+    # Face BBOX
+    xmin, xmax = min(xlist), max(xlist)
+    ymin, ymax = min(ylist), max(ylist)
+    boxW, boxH = xmax - xmin, ymax - ymin
+    bbox = xmin, ymin, boxW, boxH
+    cv2.rectangle(frame, pt1=(bbox[0] -50, bbox[1]-90), pt2=(bbox[0] + bbox[2] + 60, bbox[1] + bbox[3] + 60),
+                  color=(0, 255, 255), thickness=2)
 
-                        # Blur the mask before blending
-                        mask1 = cv2.GaussianBlur(mask1,(3,3), 10)
-                        mask2 = (255.0, 255.0, 255.0) - mask1
+    ## Blur Option
+    if br_indx:
+        face = frame[bbox[1]-90:bbox[1] + bbox[3] + 60,bbox[0] -50:bbox[0] + bbox[2] + 60]  # Extracting ROI (y:w+h,x:x+w)
+        blur = cv2.GaussianBlur(frame,(99,99),20)
+        blur[bbox[1]-90:bbox[1] + bbox[3] + 60, bbox[0] -50:bbox[0] + bbox[2] + 60] = face
+        frame = blur
+        # frame = cv2.bitwise_and(blur, mask) + cv2.bitwise_and(frame, cv2.bitwise_not(mask)) # Merge ROI into the Blurred background
+    if cv2.waitKey(1) == ord('f'):
+        flt_indx = True
 
-                        # Perform alpha blending of the two images
-                        temp1 = np.multiply(warped_img, (mask1 * (1.0/255)))
-                        temp2 = np.multiply(frame, (mask2 * (1.0 / 255)))
-                        output = temp1 + temp2
-                        cv2.imshow('output', temp1)
-                    frame = output = np.uint8(output)
+    if cv2.waitKey(1) == ord('n'):
+        index = (index + 1) % key_length
+        filters, multi_filter_runtime = load_filter(filter_keys[index])
 
-            # Face BBOX
-            xmin, xmax = min(xlist), max(xlist)
-            ymin, ymax = min(ylist), max(ylist)
-            boxW, boxH = xmax - xmin, ymax - ymin
-            bbox = xmin, ymin, boxW, boxH
-            cv2.rectangle(frame, pt1=(bbox[0] -50, bbox[1]-90), pt2=(bbox[0] + bbox[2] + 60, bbox[1] + bbox[3] + 60),
-                          color=(0, 255, 255), thickness=2)
-            #
-            # # Blur Option
-            if br_indx:
-                face = frame[bbox[1]-90:bbox[1] + bbox[3] + 60,bbox[0] -50:bbox[0] + bbox[2] + 60]  # Extracting ROI (y:w+h,x:x+w)
-                blur = cv2.GaussianBlur(frame,(99,99),20)
-                blur[bbox[1]-90:bbox[1] + bbox[3] + 60, bbox[0] -50:bbox[0] + bbox[2] + 60] = face
-                frame = blur
-                # frame = cv2.bitwise_and(blur, mask) + cv2.bitwise_and(frame, cv2.bitwise_not(mask)) # Merge ROI into the Blurred background
-            if cv2.waitKey(1) == ord('f'):
-                flt_indx = True
+    if cv2.waitKey(1) == ord('p'):
+        index = (index - 1) % key_length
+        filters, multi_filter_runtime = load_filter(filter_keys[index])
 
-            if cv2.waitKey(1) == ord('r'):
-                br_indx = False
-                flt_indx = False
-
+    if cv2.waitKey(1) == ord('r'):
+        br_indx = False
+        flt_indx = False
 
     end = time.perf_counter()
     fps = int(1 / (end - start))
